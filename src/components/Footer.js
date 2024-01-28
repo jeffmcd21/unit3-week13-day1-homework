a2c85cbb45a296183b7a038cb421be734e056319
Jeff McDonald <jwmcdonald85@gmail.com>
2024-01-28 17:20:56 -0500

    fontawesome update

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,8 @@ const Footer = (props) => {
     //The Components Returned JSX
     return (
         <div className="Footer"> 
-            <h5>Thank you for viewing my page.</h5><br></br>
+            <h5>Thank you for viewing my page.</h5><br/>
+                <i class="fa-solid fa-biohazard fa-fade"></i>
         </div>
     )
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ const Header = (props) => {
     return <div className="Header">
         <h1>J.McDonald Project Portfolio</h1>
             <div className="head-links">
-                <a href="https://linkedin.com/in/jeffry-mcdonald-589a3681"><i class="fa-brands fa-linkedin"></i></a><br></br>
+                <a href="https://linkedin.com/in/jeffry-mcdonald-589a3681"><i class="fa-brands fa-linkedin"></i></a><br/>
                 <a href="https://github.com/jeffmcd21"><i class="fa-brands fa-square-github"></i></a>
                 
             </div>
